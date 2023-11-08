b9ae3ea65fc14e4869e94cf50f0a5c9920872830
tony tolver <andrademaresantonio00@gmail.com>
2023-11-07 21:06:09 -0600

    ADD select de listado de monedas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <script setup>
+  import { ref } from 'vue'
+
+  const monedas = ref([
+      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
+      { codigo: 'MXN', texto: 'Peso Mexicano'},
+      { codigo: 'EUR', texto: 'Euro'},
+      { codigo: 'GBP', texto: 'Libra Esterlina'},
+  ])
 
 </script>
 
 <template>
+  <div class="contenedor">
+    <h1 class="titulo"> Cotizador de<span>Criptomonedas</span></h1>
 
+    <div class="contenido">
+      <form class="formulario">
+        <label for="moneda">Moneda:</label>
+        <select id="moneda">
+          <option 
+            v-for="(moneda) in monedas" 
+            :key="moneda.codigo" 
+            :value="moneda.codigo"
+          >
+            {{ moneda.texto }}
+          </option>
+        </select>
+      </form>
+    </div>
 
+  </div>
 </template>
